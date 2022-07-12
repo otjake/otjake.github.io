76d5fbc36520fb7f796ca7ce05375af9f8dc65cc
Obodomechine Jake <jake@curacel.ai>
2022-07-12 13:15:07 +0100

    First commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+  document.getElementById("offcanvas_menu").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("offcanvas_menu").style.width = "0";
+}
